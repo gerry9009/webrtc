dfe014acf4aa168d55e5b8986181b07b0697c60c
Buglyo Gergo <buglyogergo@Buglyo-MacBook-Pro.local>
2023-01-03 20:32:40 +0100

    changed design in home page

--- src/components/1_home/Home.jsx
+++ src/components/1_home/Home.jsx
@@ -10,7 +10,10 @@ const Home = () => {
     <div className="home">
       <img className="home-img" src={Picture} alt="Man doing video chat" />
       <div className="home-container">
-        <h2 className="home-heading">Let's start the fun</h2>
+        <h2 className="home-heading">Let's start the meeting</h2>
+        <p className="home-paragraph">
+          Make video chat with your friend wherever you want
+        </p>
         <Link to="/profile">
           <button className="home-btn">Start</button>
         </Link>
